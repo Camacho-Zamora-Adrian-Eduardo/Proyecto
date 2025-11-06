5429e7cbe9ce25d5d6b1e52b28110adb7ccb0c91
Camacho-Zamora-Adrian-Eduardo <24308060610609@cetis61.edu.mx>
2025-11-06 09:22:20 -0500

    avances

--- app.py
+++ app.py
@@ -6,6 +6,35 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/analisisdecomida")
+def analisis():
+    return render_template("analisis.html")
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+@app.route('/registrame', methods= ("GET", "POST"))
+def registro():
+    error = None
+    if request.method == "POST":
+        nombreCompleto = request.form["nombreCompleto"]
+        fecha = request.form["fecha"]
+        correo = request.form["correo"]
+        password = request.form["contraseña"]
+        
+    if error != None:
+        flash(error)
+        return render_template("crear.html")
+    else:
+        flash(f"Tu cuenta se a creado {nombreCompleto}")
+        return render_template("inicio.html")
+
+
+
+
+
+
 
 
 
