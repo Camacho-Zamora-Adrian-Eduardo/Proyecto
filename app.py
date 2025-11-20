9f25ed0d03b909889d5ebf653377d30b5371b750
= <=>
2025-11-20 09:20:17 -0500

    calculadora macro casi terminada

--- app.py
+++ app.py
@@ -41,15 +41,82 @@ def validasesion():
 def analisis():
     return render_template("analisis.html")
 
-@app.route("/calculadora")
-def calculadora():
-    return render_template("calculadora.html")
+@app.route("/calculadoraGET")
+def calculadoraGET():
+    return render_template("calculadoraGET.html")
 
-@app.route("/calcular")
-def calcular():
-    error=None
+@app.route("/calculadoraIMC")
+def calculadoraIMC():
+    return render_template("calculadoraIMC.html")
+
+
+
+@app.route("/calculadoraMACRO", methods=['GET', 'POST'])
+def calculadoraMACRO():
+    return render_template("calculadoraMACRO.html")
+
+@app.route("/macroscalculados")
+def macroscalculados():
     
-    return render_template("calculadora.html")
+    if request.method == 'POST':
+        peso = float(request.form['peso'])
+        altura = float(request.form['altura'])
+        edad = int(request.form['edad'])
+        genero = request.form['genero']
+        actividad = request.form['actividad']
+        objetivo = request.form['objetivo']
+        calorias = int(request.form['calorias'])
+
+        tdee, proteinas, carbohidratos, grasas = calcular_macros(
+            peso, altura, edad, genero, actividad, objetivo, calorias)
+
+        return render_template('calculadoraMACRO.html', tdee=tdee, proteinas=proteinas,
+                                carbohidratos=carbohidratos, grasas=grasas,
+                                peso=peso, altura=altura, edad=edad,
+                                genero=genero, actividad=actividad,
+                                objetivo=objetivo, calorias=calorias)
+
+
+def calcular_macros(peso, altura, edad, genero, actividad, objetivo, calorias):
+    if genero == 'hombre':
+        bmr = 88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * edad)
+    else:
+        bmr = 447.593 + (9.247 * peso) + (3.098 * altura) - (4.330 * edad)
+    
+    if actividad == 'sedentario':
+        tdee = bmr * 1.2
+    elif actividad == 'ligero':
+        tdee = bmr * 1.375
+    elif actividad == 'moderado':
+        tdee = bmr * 1.55
+    elif actividad == 'intenso':
+        tdee = bmr * 1.725
+    else:
+        tdee = bmr * 1.9
+
+    if objetivo == 'perder':
+        tdee -= calorias  
+    elif objetivo == 'ganar':
+        tdee += calorias  
+
+
+    proteinas = 0.2 * tdee / 4  
+    carbohidratos = 0.5 * tdee / 4  
+    grasas = 0.3 * tdee / 9  
+
+    return tdee, proteinas, carbohidratos, grasas
+
+
+
+@app.route("/calculadoraPI")
+def calculadoraPI():
+    return render_template("calculadoraPI.html")
+
+@app.route("/calculadoraTMB")
+def calculadoraTMB():
+    return render_template("calculadoraTMB.html")
+    
+
 
 @app.route("/registro")
 def registro():
